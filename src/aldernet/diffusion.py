867fcda21426c2787cdc9dcdb23d9f1406a748e2
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-02-24 07:26:40 +0100

    Improved plotting

--- src/aldernet/diffusion.py
+++ src/aldernet/diffusion.py
@@ -2,68 +2,66 @@
 
 # pylint: disable=R0801
 
-# Standard library
-import socket
-
 # Third-party
 import matplotlib.pyplot as plt  # type: ignore
-import numpy as np
 import torch  # type: ignore
 import xarray as xr
 from denoising_diffusion_pytorch import GaussianDiffusion  # type: ignore
+from denoising_diffusion_pytorch import Trainer  # type: ignore
 from denoising_diffusion_pytorch import Unet  # type: ignore
 
+# def show(img):
+#     npimg = img.numpy()
+#     plt.imshow(np.transpose(npimg, (1, 2, 0)), interpolation="nearest")
 
-def show(img):
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)), interpolation="nearest")
-
-
-zoom = ""
-hostname = socket.gethostname()
-if "tsa" in hostname:
-    data_train = xr.open_zarr(
-        "/scratch/sadamov/pyprojects_data/aldernet/" + zoom + "/data_train.zarr"
-    )
-    data_valid = xr.open_zarr(
-        "/scratch/sadamov/pyprojects_data/aldernet/" + zoom + "/data_valid.zarr"
-    )
-elif "nid" in hostname:
-    data_train = xr.open_zarr(
-        "/scratch/e1000/meteoswiss/scratch/sadamov/pyprojects_data/aldernet/"
-        + zoom
-        + "/data_train.zarr"
-    )
-    data_valid = xr.open_zarr(
-        "/scratch/e1000/meteoswiss/scratch/sadamov/pyprojects_data/aldernet/"
-        + zoom
-        + "/data_valid.zarr"
-    )
+data_train = xr.open_zarr(
+    "/scratch/e1000/meteoswiss/scratch/sadamov/pyprojects_data/aldernet/data_final/"
+    + "/data_train.zarr"
+)
+data_valid = xr.open_zarr(
+    "/scratch/e1000/meteoswiss/scratch/sadamov/pyprojects_data/aldernet/data_final/"
+    + "/data_valid.zarr"
+)
 
 data_img = (
     data_train.ALNU.expand_dims({"channel": 1})
-    .isel(y=slice(400, 464), x=slice(400, 464), valid_time=slice(0, 16))
+    .isel(y=slice(400, 464), x=slice(400, 464), valid_time=slice(1000, 1400))
     .transpose("valid_time", "channel", "y", "x")
     .to_numpy()
-)
+).copy()
 
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
+# Get gpu device for training.
+device = "cuda"
 
-model = Unet(dim=64, dim_mults=(1, 2, 4, 8, 16, 32, 64), channels=1).cuda()
+model = Unet(dim=64, dim_mults=(1, 2, 4, 8), channels=1).cuda()
 diffusion = GaussianDiffusion(
-    model, image_size=64, timesteps=1000, loss_type="l1"
+    model, image_size=64, timesteps=100000, loss_type="l1"
 ).cuda()
 
 training_images = torch.from_numpy(data_img).cuda()  # pylint: disable=E1101
-loss = diffusion(training_images).cuda()
-loss.backward()
+
+trainer = Trainer(
+    diffusion,
+    training_images,
+    train_batch_size=32,
+    train_lr=8e-5,
+    train_num_steps=700000,  # total training steps
+    gradient_accumulate_every=2,  # gradient accumulation steps
+    ema_decay=0.995,  # exponential moving average decay
+    amp=True,  # turn on mixed precision
+)
+
+trainer.train()
 
 sampled_images = diffusion.sample(batch_size=4).cuda()
 
-show(training_images.cpu()[0])
-show(sampled_images.cpu()[0])
+# show(training_images.cpu()[0])
+# show(sampled_images.cpu()[0])
+
+plt.imsave("input1.png", training_images.cpu()[0][0, :, :])
+plt.imsave("input2.png", training_images.cpu()[100][0, :, :])
+plt.imsave("input3.png", training_images.cpu()[200][0, :, :])
+plt.imsave("input4.png", training_images.cpu()[300][0, :, :])
 
 plt.imsave("sample1.png", sampled_images.cpu()[0][0, :, :])
 plt.imsave("sample2.png", sampled_images.cpu()[1][0, :, :])
--- src/aldernet/plots.py
+++ src/aldernet/plots.py
@@ -19,45 +19,69 @@ from PIL import Image
 from pyprojroot import here  # type:ignore
 
 # %%
+INPUT = "ml"
+health_impact_groups = True
 output_path = os.path.join(str(here()), "output/gif/")
+output_name = "model_" + INPUT + "_2022_100-150_categ.gif"
+scratch = os.environ["SCRATCH"]
 
 # %%
-ds = xr.open_zarr(
-    "/scratch/sadamov/pyprojects_data/aldernet/no_threshold/data_valid.zarr"
-)
-# ds = xr.open_dataset(str(here()) + "/data/pollen_ml.nc")
+if INPUT == "cosmo":
+    ds = xr.open_zarr(scratch + "/pyprojects_data/aldernet/data_final/data_valid.zarr")
+else:
+    ds = xr.open_dataset(str(here()) + "/data/pollen_ml.nc")
 
 # %%
-with open(str(here()) + "/data/scaling.txt", "r", encoding="utf-8") as f:
-    lines = [line.rstrip() for line in f]
-    center = float(lines[0].split(": ")[1])
-    scale = float(lines[1].split(": ")[1])
+if INPUT == "cosmo":
+    with open(str(here()) + "/data/scaling.txt", "r", encoding="utf-8") as f:
+        lines = [line.rstrip() for line in f]
+        center = float(lines[0].split(": ")[1])
+        scale = float(lines[1].split(": ")[1])
 
-ds["ALNU"].values = np.maximum(0, ds["ALNU"].values * scale + center)
+        ds["ALNU"].values = np.maximum(0, ds["ALNU"].values * scale + center)
 
 # %%
-for valid_time in range(999, len(ds.ALNU.valid_time)):
+if health_impact_groups:
+    ds["ALNU"].values = np.digitize(ds["ALNU"].values, bins=[1, 10, 70, 250])
+
+
+# %%
+# for valid_time in range(999, len(ds.ALNU.valid_time)):
+for index in range(100, 150):
     plot1 = ds.psy.plot.mapplot(
         name="ALNU",
-        valid_time=valid_time,
+        valid_time=index,
         sort=["valid_time"],
-        title="Alder Pollen in the Alps",
-        titlesize=15,
+        title="Alder Pollen in the Alpine Region on "
+        + str(ds.valid_time[index].values)[0:13],
+        titlesize=14,
         lakes=True,
         borders=True,
         rivers=True,
         grid_color="white",
         cticksize=8,
+        clabel="Health Impact Categories",
         grid_labelsize=8,
         projection="robin",
-        cmap="RdBu_r",
+        cmap="viridis",
     )
 
-    colorbar = list(range(0, 1001, 50))
-    plot1.update(bounds=colorbar, cticks=colorbar)
+    if health_impact_groups:
+        colorbar_ticks = [0.5, 1.5, 2.5, 3.5, 4.5]
+        colorbar_bounds = list(range(0, 6, 1))
+        colorbar_tickslabel = ["nothing", "weak", "medium", "strong", "very strong"]
+        plot1.update(
+            bounds=colorbar_bounds,
+            cticks=colorbar_ticks,
+            cticklabels=colorbar_tickslabel,
+        )
+    else:
+        colorbar_bounds = list(range(0, 1001, 1))
+        colorbar_ticks = [float(i) for i in colorbar_bounds]
+        plot1.update(bounds=colorbar_bounds, cticks=colorbar_ticks)
 
     plt.ioff()
-    plot1.export(output_path + "/map" + str(valid_time).zfill(4) + ".png")
+    plot1.export(output_path + "/map" + str(index).zfill(4) + ".png")
     plt.ion()
     plt.close()
 
@@ -66,7 +90,7 @@ for valid_time in range(999, len(ds.ALNU.valid_time)):
 frames = [Image.open(image) for image in sorted(glob.glob(f"{output_path}/*.png"))]
 frame_one = frames[0]
 frame_one.save(
-    output_path + "/my_awesome.gif",
+    output_path + output_name,
     format="GIF",
     append_images=frames,
     save_all=True,
