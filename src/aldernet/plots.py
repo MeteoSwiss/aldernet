fcbb51c712b1b755e70f93205941b99bfeaa262e
Simon Adamov <simon.adamov@mailbox.org>
2023-03-10 13:41:32 +0100

    new license

--- src/aldernet/plots.py
+++ src/aldernet/plots.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 MeteoSwiss, contributors listed in AUTHORS
+
+# Distributed under the terms of the BSD 3-Clause License.
+
+# SPDX-License-Identifier: BSD-3-Clause
+
 """Create a Season GIF Animation."""
 
 # pylint: disable=W0611
--- src/aldernet/utils.py
+++ src/aldernet/utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022 MeteoSwiss, contributors listed in AUTHORS
+
+# Distributed under the terms of the BSD 3-Clause License.
+
+# SPDX-License-Identifier: BSD-3-Clause
+
 """Train a cGAN with UNET Architecture.
 
 To create realistic Images of Pollen Surface Concentration Maps.
